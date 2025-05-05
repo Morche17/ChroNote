b7f193f64cd13b8d1b73dfff2187edd713c84a3f
Emanuel Tavares <tavares2003@tutanota.com>
2025-05-05 07:30:03 -0700

    Actualización en configuración de rutas. Para acceder a ciertas rutas los usuarios requieren ser autenticados.

--- api_bd/config/routes.rb
+++ api_bd/config/routes.rb
@@ -1,7 +1,26 @@
+# Rails.application.routes.draw do
+#   namespace :api, defaults: { format: 'json' } do
+#     namespace :v1 do
+#       post 'login', to: 'sessions#create'
+#       delete 'logout', to: 'sessions#destroy'
+      
+#       resources :usuarios do
+#         resources :temas, shallow: true do
+#           resources :notas, shallow: true
+#         end
+#       end
+#     end
+#   end
+# end
+
 Rails.application.routes.draw do
   namespace :api, defaults: { format: 'json' } do
     namespace :v1 do
-      resources :usuarios do
+      post 'login', to: 'sessions#create'
+      resources :usuarios, only: [:create]
+      
+      # Rutas que requieren autenticación
+      resources :usuarios, except: [:create] do
         resources :temas, shallow: true do
           resources :notas, shallow: true
         end
