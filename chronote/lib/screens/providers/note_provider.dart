ffc234bf554fc82d60346d10398bcccf560ed57d
JuanRoman01 <al22760245@ite.edu.mx>
2025-05-08 16:15:28 -0700

    Provedor modificado para trabajar con un arreglo de objetos en vez de un arreglo de cadenas

--- chronote/lib/screens/providers/note_provider.dart
+++ chronote/lib/screens/providers/note_provider.dart
@@ -1,24 +1,40 @@
 import 'package:flutter/material.dart';
 import 'package:chronote/screens/models/note.dart';
+import 'package:chronote/services/theme_service.dart';
+import 'package:chronote/screens/models/tema.dart';
 
 class NoteProvider extends ChangeNotifier {
-  List<String> themes = ['Trabajo', 'Personal', 'Estudio'];
+  List<Tema> themes = []; // Mantenemos la lista como String
   List<Note> notes = [];
 
-  void addNote(String name, String content, List<String> selectedThemes) {
+  // Cargar temas desde el backend
+  Future<void> fetchThemes(int userId) async {
+    try {
+      final temas = await ThemeService.viewThemes(userId);
+      themes = temas; // Ya es List<Tema>
+      notifyListeners();
+    } catch (e) {
+      print("Error al cargar temas: $e");
+    }
+  }
+
+  /* void addNote(String name, String content, List<String> selectedThemes) {
     for (var theme in selectedThemes) {
-      if (!themes.contains(theme)) {
-        themes.add(theme);
+      // Si el tema no existe en la lista, lo agregamos
+      if (!themes.any((t) => t.nombre == theme)) {
+        themes.add(Tema(id: -1, nombre: theme)); // id -1 indica que es local
       }
       notes.add(Note(name: name, content: content, theme: theme));
     }
     notifyListeners();
   }
 
+
   void addTheme(String theme) {
-    if (!themes.contains(theme)) {
-      themes.add(theme);
+    if (!themes.any((t) => t.nombre == theme)) {
+      themes.add(Tema(id: -1, nombre: theme));
       notifyListeners();
     }
   }
+  */
 }
