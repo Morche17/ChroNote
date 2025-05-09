33278f7e656b3bf35739eb291ea8a25d48e251c4
JuanRoman01 <al22760245@ite.edu.mx>
2025-05-09 16:30:11 -0700

    Función añadida para insertar un tema

--- chronote/lib/services/theme_service.dart
+++ chronote/lib/services/theme_service.dart
@@ -15,4 +15,22 @@ class ThemeService {
     }
   }
 
+  static Future<Map<String, dynamic>> addTheme(
+      int usuarioId, String nombre, bool poseeCalendario) async {
+    final response = await http.post(
+      Uri.parse("$baseUrl/usuarios/$usuarioId/temas"),
+      headers: {'Content-Type': 'application/json'},
+      body: jsonEncode({
+        'tema': {'nombre': nombre, 'posee_calendario': poseeCalendario }
+      }),
+    );
+
+    if (response.statusCode == 201) {
+      return jsonDecode(response.body);
+    } else {
+      throw Exception('Error en registro de tema: ${response.body}');
+    }
+  }
+
 }
+
