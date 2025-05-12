4fd076f68ecc02b4366cf1b380e473ff7269adb0
JuanRoman01 <al22760245@ite.edu.mx>
2025-05-12 05:01:18 -0700

    Inicialización de servicios para las notificaciones y su manejo de tiempo

--- chronote/lib/main.dart
+++ chronote/lib/main.dart
@@ -2,11 +2,22 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 // import 'screens/main_screen.dart';
 import 'package:chronote/screens/providers/note_provider.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:chronote/utils/session_manager.dart';
 import 'package:chronote/screens/splash_screen.dart';
+import 'package:timezone/data/latest.dart' as tz;
+
+
+
+final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+    FlutterLocalNotificationsPlugin();
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  tz.initializeTimeZones();
+  const AndroidInitializationSettings androidInit = AndroidInitializationSettings('@mipmap/ic_launcher');
+  const InitializationSettings initSettings = InitializationSettings(android: androidInit);
+  await flutterLocalNotificationsPlugin.initialize(initSettings);
   await SessionManager.init();
   runApp(
     ChangeNotifierProvider(
