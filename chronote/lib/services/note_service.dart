bcabf09cf3b9a57b804a410d92056c1bc83221ee
JuanRoman01 <al22760245@ite.edu.mx>
2025-05-12 05:05:59 -0700

    Servicio creado para elaborar notas

---
+++ chronote/lib/services/note_service.dart
@@ -0,0 +1,25 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+class NoteService {
+  static const String baseUrl = 'https://barnacle-selected-toad.ngrok-free.app/api/v1';
+
+  static Future<Map<String, dynamic>> addNote(
+      int temaId, String nombre, String descripcion, String fecha_notificacion ) async {
+    final response = await http.post(
+      Uri.parse("$baseUrl/usuarios/$temaId/temas"),
+      headers: {'Content-Type': 'application/json'},
+      body: jsonEncode({
+        'nota': {'nombre': nombre, 'descripcion': descripcion, 'fecha_notificacion': fecha_notificacion}
+      }),
+    );
+
+    if (response.statusCode == 201) {
+      return jsonDecode(response.body);
+    } else {
+      throw Exception('Error en registro de nota: ${response.body}');
+    }
+  }
+
+}
+
