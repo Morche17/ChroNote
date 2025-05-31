1e7702703216ed40c54442761ea9adda460500fd
Emanuel Tavares <tavares2003@tutanota.com>
2025-05-30 22:00:45 -0700

    Adición de métodos para ver, actualizar y eliminar temas. Mejoras en el crud del sistema.

--- chronote/lib/services/theme_service.dart
+++ chronote/lib/services/theme_service.dart
@@ -6,14 +6,19 @@ class ThemeService {
   static const String baseUrl = 'https://clever-globally-doberman.ngrok-free.app/api/v1';
 
   static Future<List<Tema>> viewThemes(int userId) async {
-    final response = await http.get(Uri.parse("$baseUrl/usuarios/$userId/temas"));
-    if (response.statusCode == 200) {
-      final List data = jsonDecode(response.body);
-      return data.map((json) => Tema.fromJson(json)).toList();
-    } else {
-      throw Exception("Fallo al obtener temas");
-    }
+  final response = await http.get(
+    Uri.parse("$baseUrl/usuarios/$userId/temas"),
+    headers: {'Content-Type': 'application/json'},
+  );
+
+  if (response.statusCode == 200) {
+    final List data = jsonDecode(response.body);
+    print(data);
+    return data.map((json) => Tema.fromJson(json)).toList();
+  } else {
+    throw Exception("Fallo al obtener temas: ${response.body}");
   }
+}
 
   static Future<Map<String, dynamic>> addTheme(
       int usuarioId, String nombre, bool poseeCalendario) async {
@@ -29,6 +34,34 @@ class ThemeService {
       return jsonDecode(response.body);
     } else {
       throw Exception('Error en registro de tema: ${response.body}');
+    }
+  }
+
+  static Future<Map<String, dynamic>> updateTheme(
+      int usuarioId, int temaId, String nombre, bool poseeCalendario) async {
+      final response = await http.patch(
+        Uri.parse("$baseUrl/usuarios/$usuarioId/temas/$temaId"),
+        headers: {'Content-Type': 'application/json'},
+        body: jsonEncode({
+          'tema': {'nombre': nombre, 'posee_calendario': poseeCalendario}
+      }),
+      );
+
+      if (response.statusCode == 200) {
+        return jsonDecode(response.body);
+      } else {
+        throw Exception('Error al actualizar el tema: ${response.body}');
+      }
+    }
+
+  static Future<void> deleteTheme(int usuarioId, int temaId) async {
+    final response = await http.delete(
+      Uri.parse("$baseUrl/usuarios/$usuarioId/temas/$temaId"),
+      headers: {'Content-Type': 'application/json'},
+    );
+
+    if (response.statusCode != 204) {
+      throw Exception('Error al eliminar el tema: ${response.body}');
     }
   }
 
